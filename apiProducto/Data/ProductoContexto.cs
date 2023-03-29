2e767c43ad82fcb2938139cfb413c95bd4c4cfe0
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-03-29 16:14:30 -0600

    controller and connection

--- apiProducto/Data/ProductoContexto.cs
+++ apiProducto/Data/ProductoContexto.cs
@@ -1,6 +1,36 @@
-﻿namespace apiProducto.Data
+﻿using apiProducto.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiProducto.Data
 {
-    public class ProductoContexto
+    public class ProductoContexto : DbContext
     {
+        public ProductoContexto(
+            DbContextOptions<ProductoContexto> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>().HasData(
+                new Producto
+                {
+                    idProducto = 1,
+                    Descripcion = "Arduino",
+                    Precio = (decimal)15000.00,
+                    Cantidad = 14,
+                    Garantia = false
+                },
+                new Producto
+                {
+                    idProducto = 2,
+                    Descripcion = "Impresora 3D",
+                    Precio = (decimal)458999.80,
+                    Cantidad = 5,
+                    Garantia = true
+                }
+                );
+        }
+        public DbSet<Producto> Productos { get; set; }
+
     }
 }
--- apiProducto/Program.cs
+++ apiProducto/Program.cs
@@ -1,8 +1,15 @@
+using apiProducto.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ProductoContexto>(
+    options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection")
+    );
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

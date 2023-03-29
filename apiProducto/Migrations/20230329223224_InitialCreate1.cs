4909cc665c724f995aceffc7055a4250dc7b44ea
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-03-29 16:35:17 -0600

    example finished

---
+++ apiProducto/Migrations/20230329223224_InitialCreate1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace apiProducto.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    idProducto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Garantia = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.idProducto);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "idProducto", "Cantidad", "Descripcion", "Garantia", "Precio" },
+                values: new object[,]
+                {
+                    { 1, 14, "Arduino", false, 15000m },
+                    { 2, 5, "Impresora 3D", true, 458999.8m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
